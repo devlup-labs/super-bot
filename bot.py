e3ab9a09661cf12efab829d1ca3e87bc4e4f4613
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2020-12-31 17:08:44 +0530

    Added clear command and changed the prefix

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = "\")
+
+@client.event
+async def on_ready():
+    print('super-bot is online!')
+
+@client.command(aliases=['hi'])
+async def hello(ctx):
+    await ctx.send(f'Hello {ctx.author.mention}, SUPER-BOT here at your service')
+
+@client.command()
+async def dob(ctx):
+    await ctx.send('I was born on the **LAST DAY OF 2020**, ready to embark a new beginning !')
+
+@client.command(aliases=['latency'])
+async def ping(ctx):
+    await ctx.send(f'`My latency is {round(client.latency*1000)} ms`')
+
+@client.command(aliases=['c'])
+@commands.has_permissions(manage_messages = True)
+async def clear(ctx, amount=2):
+    await ctx.channel.purge(limit=amount)
+  
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+client.run(TOKEN)
